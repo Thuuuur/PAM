882c91517e01593f1c42ed82fd2520442561d6c4
Arthur Rodrigues Dantas <164256979+Thuuuur@users.noreply.github.com>
2024-08-21 12:10:29 -0300

    alteração

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,29 @@
-
 import { StyleSheet, Text, View ,FlatList} from 'react-native';
 import BannerMovies from '../../components/bannerFilmes';
 import CardMovies from '../../components/cardMovies';
 import Header from '../../components/header';
 import SearchBar from '../../components/searchbar';
-import Filmes from '../../data/movies'
+import Filmes from '../../data/movies';
+import {useEffect, useState} from 'react';
 
 export default function App() {
+
+  const [movies, setMovies] = useState();
+
+  useEffect (()=>{
+
+    async function buscarFilmes(){
+      const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=a8f980bca4921bbc75ae4af44df2fb26&language=pt-BR&page=1')
+      const data = await response.json()
+      console.log(data.results)
+    }
+    
+    buscarFilmes();
+
+
+
+},[])
+
   return (
     <View style={styles.container}>
      <Header></Header>
